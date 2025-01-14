f379abbe2551dfccc7fb488c04ec98a6a1bcef8f
Théo Clouscard <theo.clouscard@insa-lyon.fr>
2025-01-14 09:19:34 +0100

    changed initialization

--- prolog.pl
+++ prolog.pl
@@ -25,7 +25,7 @@ hello :-
 initialize :-
     random_seed,          %%% use current time to initialize random number generator
     blank_mark(E),
-    asserta( board([E,E,E, E,E,E, E,E,E]) )  %%% create a blank board
+    asserta( board([E,E,E,E,E,E,E, E,E,E,E,E,E,E, E,E,E,E,E,E,E, E,E,E,E,E,E,E, E,E,E,E,E,E,E, E,E,E,E,E,E,E]) )  %%% create a blank board
     .
 
 goodbye :-
